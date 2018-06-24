0ce6bca622cffa1bef77d845e7231312bea2545f
Võ Công Khanh <vckhanhitiu1@gmail.com>
2018-06-24 10:11:56 +0700

    Compare 2 solutions for Logest Substring

---
+++ Java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+*/
+
+//Brute Force Way
+public class LongestSubstringWithoutRepeatingCharacters{
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>(); // using set to avoid duplicate characters
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+}
+
+//Using Map
+
+public class LongestSubstringWithoutRepeatingCharacters{
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
